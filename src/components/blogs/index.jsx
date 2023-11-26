9aaf8068c3831aec6b86624c8f712ab2ea345c07
Shahadat Anik <git.shahadat.anik@gmail.com>
2023-11-26 17:21:19 +0600

    Refactor blog card layout in index.jsx

--- src/components/blogs/index.jsx
+++ src/components/blogs/index.jsx
@@ -9,32 +9,28 @@ import {
 } from '@chakra-ui/react';
 import { BanglaHeading } from '../../customDesign';
 import { blogData } from './blogsData';
-// import Footer from '../utils/footer';
 export default function Index() {
   const { colorMode } = useColorMode();
   return (
-    <SimpleGrid columns={[1, 2, 3]} spacing={4} mx={2}>
+    <SimpleGrid columns={[1, 1, 3]} spacing={4} mx={2}>
       {blogData.map(blog => (
         <Card
           key={blog.number}
+          as={Link}
           bg={colorMode === 'light' ? 'gray.200' : 'gray.600'}
+          rel="noopener noreferrer"
+          href={`/${blog.englishName}`}
+          _hover={{ textDecoration: 'none' }}
         >
-          <Link
-            rel="noopener noreferrer"
-            key={blog.number}
-            href={`/${blog.englishName}`}
-            _hover={{ textDecoration: 'none' }}
-          >
-            <CardHeader>
-              <BanglaHeading as={'h1'}>{blog.name}</BanglaHeading>
-            </CardHeader>
-            <CardBody>
-              <Text>
-                {blog.date} <span>&emsp;</span>/ <span>&emsp;</span>
-                {blog.writer}
-              </Text>
-            </CardBody>
-          </Link>
+          <CardHeader>
+            <BanglaHeading as={'h1'}>{blog.name}</BanglaHeading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              {blog.date} <span>&emsp;</span>/ <span>&emsp;</span>
+              {blog.writer}
+            </Text>
+          </CardBody>
         </Card>
       ))}
     </SimpleGrid>
